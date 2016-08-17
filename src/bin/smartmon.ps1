ec44af71d10057b22ba2bc1a6e77d5260ef4b8f0
Luke Murphey <Luke@LukeMurphey.net>
2016-08-17 14:04:25 -0500

    Updating the smartmon Powershell with various changes:
    1) Making it work with present directory that Splunk calls it with
    2) It now uses the correct architecture (64-bit or 32-bit)

--- src/bin/smartmon.ps1
+++ src/bin/smartmon.ps1
@@ -1,10 +1,23 @@
+# Determine if this is a 32-bit or 64-bit OS
+$arch = "win64"
+
+if ($Env:PROCESSOR_ARCHITECTURE -eq "x86") {
+    if ($Env:PROCESSOR_ARCHITEW6432) {
+        $arch = "win32"
+    }
+}
+
+# Create a reference to the smartctl command
+$smartctl = $Env:SPLUNK_HOME + "\etc\apps\smartmon\bin\$arch\smartctl.exe"
+
 # Get the list of drives
-$drives = .\win32\smartctl.exe --scan
+$drives = Invoke-Expression ("& '$smartctl' --scan")
 
 # Break up the drives list into an array
 $drives_list = $drives.Split("{\n}")
 
+# Run smartctl on each disk
 for($i=0; $i -lt $drives_list.Count; $i++){
     $disk = $drives_list[$i].Split("{ }")[0]
-    .\win32\smartctl.exe -a $disk
+    Invoke-Expression ("& '$smartctl' -a $disk")
 }
