d376773237a804fb79471d175d1107647667db22
Luke Murphey <Luke@LukeMurphey.net>
2016-08-17 14:01:38 -0500

    Using a variable for smartmon cmd

--- src/bin/smartmon.cmd
+++ src/bin/smartmon.cmd
@@ -6,12 +6,15 @@ IF %PROCESSOR_ARCHITECTURE% == x86 (
   IF NOT DEFINED PROCESSOR_ARCHITEW6432 Set ARCH=win32
   )
 
-:: Get the list of disks
-SET COMMAND_TO_RUN="%SPLUNK_HOME%\etc\apps\smartmon\bin\%ARCH%\smartctl.exe" --scan
+:: Create a reference to the smartctl command
+SET SMARTCTL=%SPLUNK_HOME%\etc\apps\smartmon\bin\%ARCH%\smartctl.exe
+
+:: Get the list of drives
+SET COMMAND_TO_RUN="%SMARTCTL%" --scan
 
 :: Run smartctl on each disk
 for /f "usebackq tokens=*" %%a in (`%COMMAND_TO_RUN%`) do (
 	for /f "tokens=1 delims= " %%b in ("%%a") do (
-		"%SPLUNK_HOME%\etc\apps\smartmon\bin\%ARCH%\smartctl.exe" -a %%b
+		"%SMARTCTL%" -a %%b
 	)
 )
